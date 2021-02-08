680b67500ddf1ee89c516c3094077b566aebd2d7
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2021-02-09 12:34:57 +1300

    conditionals

---
+++ zoo.py
@@ -0,0 +1,18 @@
+# figures out whether a person pays adult or child price at the zoo
+
+# set child age to 13
+CHILD_AGE = 13
+
+# ask for age
+age = int(input("How old are you?"))
+
+# check age and display correct price
+if age < CHILD_AGE :
+    print("You pay the child price")
+
+print("Welcome to the zoo")
+
+
+
+
+
