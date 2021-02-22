0ba06636fe1d27c9b08e0c9e1cdb0ce15363ffc1
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2021-02-23 11:19:32 +1300

    Checks eligibility as blood donor

--- blood_donor.py
+++ blood_donor.py
@@ -1,3 +1,20 @@
 # decides whether person is eligible to be donor
 
+# constants
+MIN_AGE = 18
+MIN_WEIGHT = 45
+
+
+# ask user for age and weight
+age = int(input("Enter your age: "))
+weight = int(input("Enter your weight in kg: "))
+
+# check if eligible to be donor
+# both age and weight must OK to be eligible
+
+if age >= MIN_AGE and weight >= MIN_WEIGHT :
+    print("You are eligibile")
+else:
+    print("You are not eligible")
+
 
