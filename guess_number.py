f2b86541d4112937b134ed874f016ef6e27b68c1
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2021-02-22 21:33:24 +1300

    used random number for num to guess

--- guess_number.py
+++ guess_number.py
@@ -1,18 +1,23 @@
 # guessing game
 
+# get code to generate random numbers
+import random
+
 # set Boolean to keep asking until correct guess
 keep_guessing = True
 
+# set our number to guess
+secret_number = random.randint(0,10)
 
 while keep_guessing == True:
     # ask for guess
     number = int(input("Enter a number between 1 and 10: "))
 
     # check guess
-    if number == 6 :
+    if number == secret_number :
         print("\nThat's right") 
         keep_guessing = False
-    elif number > 6 :
+    elif number > secret_number :
         print("\nToo high, guess again")
     else:
         print("\nToo low, guess again")
