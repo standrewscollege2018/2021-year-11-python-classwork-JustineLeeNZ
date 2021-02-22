90e437deaa27194d96ecc1e8943f956b96d6065e
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2021-02-22 21:42:28 +1300

    added comments

--- guess_number.py
+++ guess_number.py
@@ -9,20 +9,28 @@ keep_guessing = True
 # set our number to guess
 secret_number = random.randint(0,10)
 
+# keep asking for guess until guess is correct
 while keep_guessing == True:
     # ask for guess
     number = int(input("Enter a number between 1 and 10: "))
 
     # check guess
+    # correct answer
     if number == secret_number :
         print("\nThat's right") 
+        # stop asking for guesses
         keep_guessing = False
+        
+    # guess too high
     elif number > secret_number :
         print("\nToo high, guess again")
+    
+    # guess too low
     else:
         print("\nToo low, guess again")
         
-        
+
+# number successfully guessed, show game stats     
 print("Game over")
 
 
