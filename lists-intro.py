c8bf8ce708993b8b91623f72f422fc480ed68087
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2021-03-01 14:37:18 +1300

    Intro to lists

---
+++ lists-intro.py
@@ -0,0 +1,37 @@
+# This program demonstrates how lists work
+
+# For lists, we use square brackets
+# Items in lists are separated by commas
+# String needs to have speech marks around it
+
+
+# To print a list
+
+
+# To print an individual item from a list, we use
+# its index (position in the list)
+
+
+# To add something to the end of a list, use append()
+
+
+# To add something in a specific position, use insert()
+
+
+# Lists are mutable (changeable)
+# To change the value of an item in a list, we just
+# over-write it
+
+
+# len(list) returns the number of items in a list
+
+
+# There are two ways we can loop through a list to
+# print out the items
+
+# 1. a simple for loop with no range
+
+
+# 2. Use range() to get numbered list
+# Use len(list) to get how many times to loop
+
