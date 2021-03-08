6709ad3329b31c8c1bbb9ccdf916804390ff00b9
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2021-03-09 12:03:28 +1300

    added loop so asks for multiple speeders

--- speeder.py
+++ speeder.py
@@ -1,15 +1,19 @@
 # system for police officer who catches speeders
 
+keep_asking = True
+while keep_asking:
+    name = input("Enter a name")
+    if name == "end" :
+        keep_asking = False
+    else:
+        speed_limit = int(input("Enter current limit"))
+        current_speed = int(input("Enter speed"))
 
-name = input("Enter a name")
-speed_limit = int(input("Enter current limit"))
-current_speed = int(input("Enter speed"))
-
-if current_speed - speed_limit > 29 :
-    print("Fine is $180")
-elif current_speed - speed_limit >= 19 :
-    print("Fine is $130")
-elif current_speed - speed_limit >= 10 :
-    print("Fine is $80")
-else:
-    print("Fine is $30")
+        if current_speed - speed_limit > 29 :
+            print("Fine is $180")
+        elif current_speed - speed_limit >= 19 :
+            print("Fine is $130")
+        elif current_speed - speed_limit >= 10 :
+            print("Fine is $80")
+        else:
+            print("Fine is $30")
